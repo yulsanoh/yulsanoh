2efc46399bc2856f37a408d76b01bc4e4bdb70fe
yulsan oh <sanyulixxx@gmail.com>
2021-03-13 00:08:04 +0900

    update boiler-plate

--- exercise/reactjs/boiler-plate/models/User.js
+++ exercise/reactjs/boiler-plate/models/User.js
@@ -6,6 +6,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -60,9 +61,36 @@ userSchema.pre("save", function (next) {
 });
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
-  bcrypt.compare(plainpassword, this.password, function (err, isMatch) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
     cb(null, isMatch); // isMatch는 boolean형
+  });
+};
+
+userSchema.methods.genarateToken = function (cb) {
+  var user = this;
+  // jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  // user._id + 'secretToken' = token 이므로 둘 다 기억해줘야함
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  // 토큰을 decode 한다.
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+    user.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
   });
 };
 
