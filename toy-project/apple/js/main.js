84f23e87a1edf989066ad8fd19f703a415773ff2
yulsan oh <sanyulixxx@gmail.com>
2021-05-29 17:03:21 +0900

    update

---
+++ toy-project/apple/js/main.js
@@ -0,0 +1,56 @@
+(() => {
+
+    // 객체가 4개인 이유는 scroll-section의 갯수와 동일
+    const sceneInfo = [
+        {
+            // index 0
+            type: 'sticky',
+            heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+            scrollHeight: 0,
+            objs: {  // HTML 요소들의 객체
+                container: document.querySelector('#scroll-section-0')
+            }
+        },
+        {
+            // index 1
+            type: 'normal',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-1')
+            }
+        },
+        {
+            // index 2
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-2')
+            }
+        },
+        {
+            // index 3
+            type: 'sticky',
+            heightNum: 5,
+            scrollHeight: 0,
+            objs: {
+                container: document.querySelector('#scroll-section-3')
+            }
+        },
+    ];
+
+    function setLayout() {
+        // 각 scroll-section의 height 세팅
+        for (let i = 0; i < sceneInfo.length; i++) {
+            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        }
+
+        console.log(sceneInfo);
+    }
+
+    window.addEventListener('resize', setLayout);
+
+    setLayout();
+})();
