ea1cb60c670e98ff32a888cd6a86260c260e9ade
yulsan oh <sanyulixxx@gmail.com>
2021-06-02 23:46:11 +0900

    update

--- toy-project/apple/js/main.js
+++ toy-project/apple/js/main.js
@@ -1,5 +1,9 @@
 (() => {
 
+    let yOffset = 0;  // window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0;  // 현재 스크롤 위치보다 이전 스크롤 섹션들 스크롤 높이의 합
+    let currentScene = 0;  // 현재 활성화 된 Scene(Scroll-section)
+
     // 객체가 4개인 이유는 scroll-section의 갯수와 동일
     const sceneInfo = [
         {
@@ -8,7 +12,14 @@
             heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
             scrollHeight: 0,
             objs: {  // HTML 요소들의 객체
-                container: document.querySelector('#scroll-section-0')
+                container: document.querySelector('#scroll-section-0'),
+                messageA: document.querySelector('#scroll-section-0 .main-message-a'),
+                messageB: document.querySelector('#scroll-section-0 .main-message-b'),
+                messageC: document.querySelector('#scroll-section-0 .main-message-c'),
+                messageD: document.querySelector('#scroll-section-0 .main-message-d')
+            },
+            values: {
+                messageA_opacity: [0, 1]
             }
         },
         {
@@ -47,10 +58,73 @@
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
 
-        console.log(sceneInfo);
+        yOffset = window.pageYOffset;
+
+        let totalScrollHeight = 0;
+        for (let i = 0; i < sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if (totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break;
+            }
+        }
+
+        document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
-    window.addEventListener('resize', setLayout);
+    function calcValues(values, currentYOffset) {
+        
+    }
 
-    setLayout();
+    function playAnimation() {
+        const objs = sceneInfo[currentScene].objs;
+        const values = sceneInfo[currentScene].values;
+        const currentYOffset = yOffset - prevScrollHeight;
+
+        console.log(`currentScene: ${currentScene} currentYOffset: ${currentYOffset}`);
+
+        switch (currentScene) {
+            case 0:
+                // console.log('0 play');
+                let messageA_opacity_0 = values.messageA_opacity[0];
+                let messageA_opacity_1 = values.messageA_opacity[1];
+                console.log( calcValues(values.messageA_opacity, currentYOffset) )
+                break;
+            case 1:
+                // console.log('1 play');
+                break;
+            case 2:
+                // console.log('2 play');
+                break;
+            case 3:
+                // console.log('3 play');
+                break;
+        }
+    }
+
+    function scrollLoop() {
+        prevScrollHeight = 0;
+        for (let i = 0; i < currentScene; i++) {
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+
+        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            currentScene++;
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
+        }
+
+        if (yOffset < prevScrollHeight) {
+            if (currentScene === 0) return;
+            currentScene--;
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
+        }
+        playAnimation();
+    }
+
+    window.addEventListener('scroll', () => {
+        yOffset = window.pageYOffset;
+        scrollLoop();
+    })
+    window.addEventListener('load', setLayout);
+    window.addEventListener('resize', setLayout);
 })();
