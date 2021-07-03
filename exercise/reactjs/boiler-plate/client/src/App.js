3fda4ec0663d0a814ae933892b43e580d8358081
yulsan oh <sanyulixxx@gmail.com>
2021-07-03 18:26:46 +0900

    finish boiler-plate class

--- exercise/reactjs/boiler-plate/client/src/App.js
+++ exercise/reactjs/boiler-plate/client/src/App.js
@@ -10,6 +10,7 @@ import {
 import LandingPage from './components/views/LandingPage/LandingPage'
 import LoginPage from './components/views/LoginPage/LoginPage'
 import RegisterPage from './components/views/RegisterPage/RegisterPage'
+import Auth from './hoc/auth'
 
 function App() {
   return (
@@ -27,9 +28,9 @@ function App() {
           {/* <Route>
                 <LandingPage />
               </Route> */}
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
         </Switch>
       </div>
     </Router>
---
+++ exercise/reactjs/boiler-plate/client/src/hoc/auth.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { auth } from '../_actions/user_action';
+
+export default function (SpecificComponent, option, adminRoute = null) {
+
+    // option:
+    // null = 아무나 출입이 가능
+    // true = 로그인 한 유저만 출입이 가능한 페이지
+    // false = 로그인 한 유저는 출입 불가능한 페이지
+
+    function AuthenticationCheck(props) {
+        const dispatch = useDispatch();
+
+        useEffect(() => {
+
+            dispatch(auth()).then(response => {
+                console.log(response)
+
+                // 로그인 하지 않은 상태
+                if(!response.payload.isAuth) {
+                    if(option) {
+                        props.history.push('/login')
+                    }
+                } else {
+                    // 로그인 한 상태
+                    if(adminRoute && !response.payload.isAdmin) {
+                        props.history.push('/')
+                    } else {
+                        if(option === false)
+                        props.history.push('/')
+                    }
+                }
+            })
+
+        }, [])
+
+        return (
+            <SpecificComponent />
+        )
+    }
+
+
+    return AuthenticationCheck
+}
